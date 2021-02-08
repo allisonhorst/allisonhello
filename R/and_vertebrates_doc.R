01d44432b42bb7825251623c22a7aa859fc6aa3a
Allison Horst <ahorst@ucsb.edu>
2021-02-08 11:45:17 -0800

    Initial commit

---
+++ R/and_vertebrates_doc.R
@@ -0,0 +1,8 @@
+#' Andrews Forest Mack Creek vertebrates
+#'
+#' Size and location information for cutthroat trout and salamanders
+#'
+#' @source {Gregory, S.V. and I. Arismendi. 2020. Aquatic Vertebrate Population Study in Mack Creek, Andrews Experimental Forest, 1987 to present ver 14. Environmental Data Initiative. https://doi.org/10.6073/pasta/7c78d662e847cdbe33584add8f809165 (Accessed 2021-02-08).}
+#' \url{https://portal.edirepository.org/nis/mapbrowse?packageid=knb-lter-and.4027.14}
+
+"and_vertebrates"
